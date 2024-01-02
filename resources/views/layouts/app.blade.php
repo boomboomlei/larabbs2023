8bfca6f2225dec3f2c8e78648532b8732ea50818
boomboomlei <1846352100@qq.com>
2024-01-02 02:47:38 +0000

    话题内容显示页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>@yield('title', 'LaraBBS') - Laravel BBS论坛</title>
+  <meta name="description" content="@yield('description','larabbs 爱好者论坛')">
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
