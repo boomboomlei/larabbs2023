a9d631f7072e090f8b805b8e5aabc973da326ad1
boomboomlei <1846352100@qq.com>
2024-01-09 00:59:25 +0000

    注册用户默认头像

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,7 +73,6 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            "avatar"=>config("app.url")."/".'head-default.jpeg'
         ]);
     }
 }
